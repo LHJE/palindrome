cd7f977ef33ee507da03eaa1fc842d31bd9cd243
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-17 10:34:21 -0700

    Refactored out the helper method

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,20 +1,12 @@
 class Palindrome
 
-  def self.is_palindrome(potential_palindrome)
-    if potential_palindrome == potential_palindrome.to_s.reverse.to_i && potential_palindrome > 1000
-      true
-    else
-      false
-    end
-  end
-
   def self.get_palindromes(number)
     return_array = []
     (109..Float::INFINITY).each do |x|
       potential_palindrome = (x + x.to_s.reverse.to_i)
       if return_array.count == number
         break
-      elsif is_palindrome(potential_palindrome)
+      elsif potential_palindrome == potential_palindrome.to_s.reverse.to_i && potential_palindrome > 1000
         return_array << x
       else
         next
@@ -22,7 +14,5 @@ class Palindrome
     end
     return_array
   end
-
-
 
 end
