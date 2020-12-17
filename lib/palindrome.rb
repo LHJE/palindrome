b2babd49eeb788ed7f6bf48c7fc78b5933324b65
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-17 10:29:00 -0700

    Add helper method

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,14 +1,21 @@
 class Palindrome
 
+  def self.is_palindrome(potential_palindrome)
+    require "pry"; binding.pry
+    if condition
+      true
+    else
+      false
+    end
+  end
 
   def self.get_palindromes(number)
     return_array = []
     (109..Float::INFINITY).each do |x|
+      potential_palindrome = (x + x.to_s.reverse.to_i)
       if return_array.count == number
         break
-      elsif
-
-        require "pry"; binding.pry
+      elsif is_palindrome(potential_palindrome)
         return_array << x
       else
         next
