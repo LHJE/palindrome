c3c9fa67acfbd9216af2d5911c7d6cdf23ec43a7
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-12-17 10:18:29 -0700

    add conditional

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -3,7 +3,10 @@ class Palindrome
 
   def self.get_palindromes(number)
     (109..Float::INFINITY).each do |x|
-      require "pry"; binding.pry
+      if condition
+
+        require "pry"; binding.pry
+      end
 
     end
   end
